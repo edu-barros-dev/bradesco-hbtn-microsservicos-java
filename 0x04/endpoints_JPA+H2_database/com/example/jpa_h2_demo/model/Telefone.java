ce96582a3156d52a223072b64b0399bcf8e79cc4
Eduardo Barros <edu.barros.dev@gmail.com>
2025-12-16 22:52:06 -0300

    Task 2 Definindo Serviços para a API REST usando Spring Data JPA + H2 Database + H2 Database - Java Microsserviços - Spring Data JPA + H2 Database

---
+++ 0x04/endpoints_JPA+H2_database/com/example/jpa_h2_demo/model/Telefone.java
@@ -0,0 +1,53 @@
+package com.example.jpa_h2_demo.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+@Entity
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String DDD;
+    private String numero;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDDD() {
+        return DDD;
+    }
+
+    public void setDDD(String DDD) {
+        this.DDD = DDD;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
+
