6cf5d22dcba6f919184398d789ccb7024e040266
Eduardo Barros <edu.barros.dev@gmail.com>
2025-12-18 23:44:11 -0300

    Task 0 Criando um aplicação API REST Calculator + JUNIT - Java Microsserviços - Testes unitários em Rest API

--- 0x07/api-rest-calculator/src/main/java/com/example/calculator/CalculatorApiApplication.java
+++ 0x07/api-rest-calculator/src/main/java/com/example/calculator/CalculatorApiApplication.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package src.main.java.com.example.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
